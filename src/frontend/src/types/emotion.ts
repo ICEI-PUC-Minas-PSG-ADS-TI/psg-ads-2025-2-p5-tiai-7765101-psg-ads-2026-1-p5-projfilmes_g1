c51f505a8196753a7ae50951d12018bf8a65fb52
Melissa Duque Baía <melissaduquebaia@gmail.com>
2026-04-23 16:17:24 -0300

    Integrando dashboard de insights com a api

--- src/frontend/src/types/emotion.ts
+++ src/frontend/src/types/emotion.ts
@@ -2,3 +2,10 @@ export interface EmotionRequest {
     mood: string;
     diary: string;
 }
+export interface EmotionResponse {
+    id: string;
+    data: string;
+    nivel: number;
+    mood: string;
+    diary: string;
+}
