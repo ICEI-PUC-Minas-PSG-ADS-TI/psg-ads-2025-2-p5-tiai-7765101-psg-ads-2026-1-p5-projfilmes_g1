d126f6cc9a089e9dc7d150ffb0d1f009e0eb5b2f
Arthur Coelho de Souza <51198520+arthurcoelhosouza@users.noreply.github.com>
2026-04-25 23:39:18 -0300

    Criação do repositório e service

---
+++ src/backend/backend/Services/IBreathingService.cs
@@ -0,0 +1,9 @@
+﻿using backend.DTOs;
+
+namespace backend.Services
+{
+    public interface IBreathingService
+    {
+        Task SaveSessionAsync(string userId, BreathingRequest request);
+    }
+}
